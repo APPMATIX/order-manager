98e17ae4e0c62e17a1f982bcc32637b005c3c8b9
Kebin B Jacob <kevinparackal10@gmail.com>
2025-10-30 14:51:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4978464444-3fae6",
+  "appId": "1:871282879454:web:da4c1e2c3d61262cb64b4f",
+  "apiKey": "AIzaSyAf__b5CFr_HyE2svDEaJi9AHps_7rEHlM",
+  "authDomain": "studio-4978464444-3fae6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "871282879454"
+};
